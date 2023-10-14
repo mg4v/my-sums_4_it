5b4285fadc856fd28218aa7f2e312b0622a6df0a
mg <i@mgorya4ev.ru>
2023-10-14 20:08:34 +0300

    commit

--- tests_scripts/01_script.sh
+++ tests_scripts/01_script.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # our comment is here
-echo "##### The current directory is:"
-pwd
+mydir=$(pwd)
+echo "##### The current directory is: $mydir"
 echo "##### The user logged in is:"
 whoami
+# display user home
+echo "Home for the current user is: $HOME"
+
+# testing variables
+grade=5
+person="Adam"
+echo "$person is a good boy, he is in grade $grade"
